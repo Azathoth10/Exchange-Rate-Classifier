80f227a354d63baa612b61d0c89d1eb56c785b78
unknown <fancellilorenzo1@gmail.com>
2022-05-30 23:05:36 +0200

    Add_slope

--- ExchangeRate.r
+++ ExchangeRate.r
@@ -4,12 +4,23 @@ library(dplyr)
 library(pracma)
 
 TKs <- c("GBPUSD=x")
-start <- c("2020-01-01")
+
+start <- c("2014-01-01")
 
 df <- GetTS(TKs, start)
+
+df <- na.omit(df)
+sum(is.na(df))
+nrow(df)
+
+sum(is.na(df[1]))
 
 df <- Calc_rets(df)
 
 colnames(df) <- c("Close, Rets")
 
-df$EWM_4day <- movavg(df$Close , n=12, type='e')
+df$EWM_FAST <- movavg(df$Close , n=12, type='e')
+df$EWM_SLOW <- movavg(df$Close , n=26, type='e')
+df$MACD <- df$EWM_FAST - df$EWM_SLOW
+df$signal <- movavg(df$MACD , n=9, type='e')
+df$diffMACD <- df$MACD - df$signal
---
+++ slope.r
@@ -0,0 +1,7 @@
+Slope <- function(tser, n){
+  
+  
+  
+  
+  
+}
