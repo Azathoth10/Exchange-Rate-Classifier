4aa0fb4d7f8d7577df5ed6ea1478cbc49219f639
unknown <fancellilorenzo1@gmail.com>
2022-06-08 00:07:30 +0200

    Slope Calculator

--- ExchangeRate.r
+++ ExchangeRate.r
@@ -1,5 +1,6 @@
 source("GetTickers.r")
 source("rets.r")
+source('slope.r')
 library(dplyr)
 library(pracma)
 
@@ -17,10 +18,31 @@ sum(is.na(df[1]))
 
 df <- Calc_rets(df)
 
-colnames(df) <- c("Close, Rets")
+colnames(df) <- c("Close", "Rets")
 
 df$EWM_FAST <- movavg(df$Close , n=12, type='e')
 df$EWM_SLOW <- movavg(df$Close , n=26, type='e')
 df$MACD <- df$EWM_FAST - df$EWM_SLOW
 df$signal <- movavg(df$MACD , n=9, type='e')
 df$diffMACD <- df$MACD - df$signal
+
+n = 5
+dfr <- data.frame(df$Close)
+coeffic <- c()
+x <- c(1:5)
+
+for(i in c((n+1):(nrow(df)+1))){
+  
+  y <- dfr[((i-n):(i-1)),]
+  summ <- summary(lm(y~x))
+  coeff <- summ$coefficients
+  m <- coeff[2,1]
+  
+  coeffic <- c(coeffic, m)
+  
+}
+
+angles <- atan(coeffic)
+df <- tail(df, -(n-1))
+df$slopes <- angles
+
--- slope.r
+++ slope.r
@@ -1,7 +1,25 @@
-Slope <- function(tser, n){
+Slopecalc <- function(df, tser, n){
   
+  coeffic <- c()
+  dff <- data.frame(tser)
+  x <- c(1:n)
+  for(i in c((n+1):(nrow(dff)+1))){
+    
+    y <- as.numeric(dff[(i-n):(i-1),])
+    cff <- as.numeric(as.matrix(summary(lm(y~x))$coefficients)[2,1])
+    coeffic <- c(coeffic, cff)
+    
+    print(cff)
+    
+    
+  df <- tail(df, -(n-1))
+  angles <- atan(coeffic)
+  df$slopes <- coeffic
   
-  
-  
+  return(df)
+
+    
+  }
   
 }
+
